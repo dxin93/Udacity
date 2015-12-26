7c00031f88825d442c56b4d0d56be43b6d4ef20f
dxin93 <dxin93@gmail.com>
2015-12-25 17:16:45 -0800

    Add hough peaks method

--- Introduction to Computer Vision/ps1_octave/hough_peaks.m
+++ Introduction to Computer Vision/ps1_octave/hough_peaks.m
@@ -10,14 +10,33 @@ function peaks = hough_peaks(H, varargin)
 
     %% Parse input arguments
     p = inputParser;
-    p = p.addOptional('numpeaks', 1, @isnumeric);
-    p = p.addParamValue('Threshold', 0.5 * max(H(:)));
-    p = p.addParamValue('NHoodSize', floor(size(H) / 100.0) * 2 + 1);  % odd values >= size(H)/50
-    p = p.parse(varargin{:});
+    p.addOptional('numpeaks', 1, @isnumeric);
+    p.addParamValue('Threshold', 0.5 * max(H(:)));
+    p.addParamValue('NHoodSize', floor(size(H) / 100.0) * 2 + 1);  % odd values >= size(H)/50
+    p.parse(varargin{:});
 
     numpeaks = p.Results.numpeaks;
     threshold = p.Results.Threshold;
     nHoodSize = p.Results.NHoodSize;
 
     % TODO: Your code here
+    peaks = [];
+
+    for i = 1:numpeaks
+      sorted = sort(H(:),'descend');
+      local_max = sorted(1);
+      
+      if local_max >= threshold
+        [r,t] = find(H == local_max, 1);
+        
+        NHoodY_Low = max([1 floor(r-nHoodSize(1))]);
+        NHoodY_High = min([rows(H) ceil(r+nHoodSize(1))]);
+        NHoodX_Low = max([1 floor(t-nHoodSize(2))]);
+        NHoodX_High = min([columns(H) ceil(t+nHoodSize(2))]);
+        
+        H(NHoodY_Low:NHoodY_High,NHoodX_Low:NHoodX_High) = 0;
+        
+        peaks = [peaks; r, t];
+      endif
+    endfor
 endfunction
--- Introduction to Computer Vision/ps1_octave/ps1.m
+++ Introduction to Computer Vision/ps1_octave/ps1.m
@@ -10,10 +10,13 @@ imwrite(img_edges, fullfile('output', 'ps1-1-a-1.png'));  % save as output/ps1-1
 %% 2-a
 [H, theta, rho] = hough_lines_acc(img_edges);  % defined in hough_lines_acc.m
 %% TODO: Plot/show accumulator array H, save as output/ps1-2-a-1.png
-imwrite(uint8(mat2gray(H) * 255),'output/ps1-2-a-1.png');
-
+grayH = uint8(mat2gray(H) * 255);
+imshow(grayH, 'xData', theta, 'yData', rho);
+saveas(1,'output/ps1-2-a-1.png');
 %% 2-b
-%peaks = hough_peaks(H, 10);  % defined in hough_peaks.m
+peaks = hough_peaks(H, 10);  % defined in hough_peaks.m
 %% TODO: Highlight peak locations on accumulator array, save as output/ps1-2-b-1.png
+hold on; plot(theta(peaks(:,2)), rho(peaks(:,1)),'rs'); hold off;
+saveas(1,'output/ps1-2-b-1.png');
 
 %% TODO: Rest of your code here
